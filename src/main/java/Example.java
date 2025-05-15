2e695f093f58ea29728dc2169f9e5cfafe50cbe9
Tatiana Berlenko <tatiana.berlenko@gmail.com>
2025-05-15 11:56:10 +0300

    add gradle mini project

---
+++ src/main/java/Example.java
@@ -0,0 +1,39 @@
+package com.example.robot;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Example {
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<>();
+        items.add("one");
+        items.add("two");
+        items.add("one");
+        for (String item : items) {
+            System.out.println(item);
+        }
+        int unused = 42;
+    }
+
+    public void tooManyMethods01() {}
+    public void tooManyMethods02() {}
+    public void tooManyMethods03() {}
+    public void tooManyMethods04() {}
+    public void tooManyMethods05() {}
+    public void tooManyMethods06() {}
+    public void tooManyMethods07() {}
+    public void tooManyMethods08() {}
+    public void tooManyMethods09() {}
+    public void tooManyMethods10() {}
+    public void tooManyMethods11() {}
+    public void tooManyMethods12() {}
+    public void tooManyMethods13() {}
+    public void tooManyMethods14() {}
+    public void tooManyMethods15() {}
+    public void tooManyMethods16() {}
+    public void tooManyMethods17() {}
+    public void tooManyMethods18() {}
+    public void tooManyMethods19() {}
+    public void tooManyMethods20() {}
+    public void tooManyMethods21() {}
+}
